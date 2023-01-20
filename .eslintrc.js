2bb8517a26e1165c80d64d06db59582c42eba789
xjq <lebnizjoan@gmail.com>
2023-01-20 15:20:28 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript', 'prettier'],
+  plugins: ['prettier'],
+  parser: '@typescript-eslint/parser',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
+  rules: {},
+};
---
+++ index.ts
@@ -0,0 +1,4 @@
+export function a(): void {}
+
+const b: number = 1;
+console.log(b);
